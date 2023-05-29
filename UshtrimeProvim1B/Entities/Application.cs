7a2b5772634d6855004d004b5ccad88d627115c5
EnisaBekteshiBahtiri <enisabekteshi97@gmail.com>
2023-05-29 19:46:55 +0200

    Add project files.

---
+++ UshtrimeProvim1B/Entities/Application.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using UshtrimeProvim1B.Validations;
+
+
+#nullable disable
+
+namespace UshtrimeProvim1B.Entities
+{
+    public partial class Application
+    {
+        public int Id { get; set; }
+        public int PersonId { get; set; }
+        public decimal Amount { get; set; }
+
+        [ApplicationIdValidationAttribute]
+        public string ApplicationId { get; set; }
+
+        public virtual Person Person { get; set; }
+    }
+}
---
+++ UshtrimeProvim1B/Validations/ApplicationIdValidationAttribute.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UshtrimeProvim1B.Validations
+{
+    public class ApplicationIdValidationAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            string strValue = value as string;
+            var currDate = DateTime.Now;
+            string firstSixChars = currDate.ToString("ddMMyy");
+
+            if(strValue.Length >= 6)
+            {
+                for (int i = 0; i < firstSixChars.Length; i++)
+                {
+                    if (strValue[i] != firstSixChars[i])
+                    {
+                        return false;
+                    }
+                }
+
+
+                List<string> CreditType = new List<string>() { "KON", "BLE", "MAK", "BLA" };
+
+                if (strValue.Length >= 9)
+                {
+                    var last3Chars = strValue.Substring(6, 3);
+                    if (CreditType.Contains(last3Chars))
+                    {
+                        var next5Chars = strValue.Substring(9, 5);
+                        var isNumber = next5Chars.ToArray().All(x => char.IsNumber(x));
+
+                        if (isNumber)
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                    return false;
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
